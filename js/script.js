32104d8590d5827b89b72026a7fc2034c8c34ae7
Sharjeel <saniiyou1067@gmail.com>
2023-06-18 20:57:40 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+const loader= document.querySelector(".loadingPage");
+window.addEventListener("load",function () {
+    loader.style.display="none"
+})
+
+let myButton =document.getElementById("myBtn");
+window.onscroll =function(){
+    scrollFunction();
+}
+
+function scrollFunction(){
+    if (document.body.scrollTop >20 ||document.documentElement.scrollTop > 20) {
+        myButton.style.display="block";
+
+    }
+    else{
+        myButton.style.display="none"
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop=0;
+}
+
+// <----------------------------------j-quey--------------------------------------------> 
+$(document).ready(()=>{
+    $('.font1,.dig-p,.allServicesImg').fadeOut()
+$(window).scroll(()=>{
+    $('.font1,.dig-p,.allServicesImg').fadeIn(2000);
+   
+
+})
+
+$('#container3').scroll(()=>{
+$(".client-Img").fadeIn(4000);
+
+})
+
+})
